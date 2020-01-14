d1eb3c18f484f34576691e3ebd7cc790c387dd36
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-01-14 19:13:55 +0500

    Реализовал возможность запустить игру нажав на кнопу "НАЧАТЬ ИГРУ"

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("Hello Gulp");
+
+class Game {
+  constructor(gameSettings) {
+    this.gameLaunchButton = gameSettings.gameLaunchButton;
+    this.gameField = gameSettings.gameField;
+
+    this.init();
+  }
+
+  init() {
+    this.gameLaunchButton.addEventListener("click", () => {
+      this.gameLaunchButtonHandler();
+    });
+  }
+
+  gameLaunchButtonHandler() {
+    this.gameLaunchButton.parentNode.classList.add("hide");
+    this.gameField.classList.add("gameFieldStyle");
+  }
+
+
+}
+
+const game = new Game({
+  gameLaunchButton: document.querySelector(
+    ".header-content__button-start-game"
+  ),
+  gameField: document.querySelector(".header-content__wrapper-game-fields")
+});
