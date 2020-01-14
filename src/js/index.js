aa755aa0d4b6824ef0c0bf8c73ae2c389a67ab53
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-01-14 19:26:37 +0500

    Реализовал возможность запустить таймер при клике на кнопку "НАЧАТЬ ИГРУ"

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ class Game {
   constructor(gameSettings) {
     this.gameLaunchButton = gameSettings.gameLaunchButton;
     this.gameField = gameSettings.gameField;
+    this.gameTimeDisplay = gameSettings.gameTimeDisplay;
 
     this.init();
   }
@@ -17,14 +18,25 @@ class Game {
   gameLaunchButtonHandler() {
     this.gameLaunchButton.parentNode.classList.add("hide");
     this.gameField.classList.add("gameFieldStyle");
+    this.gameTimer()
   }
 
-
+  gameTimer() {
+    const interval = setInterval(() => {
+      const time = this.gameTimeDisplay.textContent;
+      if (time <= 0) {
+        clearInterval(interval);
+      } else {
+        this.gameTimeDisplay.textContent = (time - 0.1).toFixed(1);
+      }
+    }, 100);
+  }
 }
 
 const game = new Game({
   gameLaunchButton: document.querySelector(
     ".header-content__button-start-game"
   ),
-  gameField: document.querySelector(".header-content__wrapper-game-fields")
+  gameField: document.querySelector(".header-content__wrapper-game-fields"),
+  gameTimeDisplay: document.querySelector(".header-content__time-game")
 });
